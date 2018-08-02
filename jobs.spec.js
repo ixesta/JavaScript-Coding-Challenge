0b5843711035543f789f1f041d7d0f369ae0b04b
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-08-02 16:25:49 +0100

    test for empty string

--- jobs.spec.js
+++ jobs.spec.js
@@ -3,9 +3,10 @@ const { sortingJobs } = require('./jobs');
 
 describe("sortingJobs", () => {
   // testing empty string
-  it("returns an empty array when an empty string is passed", () => {
+  it("returns an empty sequence when an empty string is passed", () => {
     const input = "";
     const actual = sortingJobs(input);
     expect(actual).to.eql([]);
   });
+
 });  
