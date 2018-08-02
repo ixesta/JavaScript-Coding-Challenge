d4289b766e6b8e651c41b8b4e8edd121ed6bebb8
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-08-02 16:59:57 +0100

    funcionality + tests

--- jobs.js
+++ jobs.js
@@ -2,12 +2,23 @@ const sortingJobs = (jobs) => {
 
   // returns an empty array when an empty string is passed
   if (jobs.length === 0) return [];
-  // turn input on array of letters by splitting it along a new line and removing character that is not a letter
+  // turn input on array of letters by splitting it along a new line and removing all characters that are not letters
   const jobsArr = jobs.split('\n').map(job => job.replace(/\W/g, ''));
 
+  // throw an error if a job depends on itself
   return jobsArr.reduce((acc, [job = '', dependency]) => {
     if (acc.includes('Error')) return acc;
     if (job === dependency) return 'Error!!! Jobs cannot depend on themselves';
+
+    // create variables to find out if the result array already has that job or its dependency
+    const alreadyAJob = acc.includes(job);
+    const alreadyDependency = acc.includes(dependency);
+
+    // throw an error if a circular dependency is found
+    if (alreadyAJob && alreadyDependency) return "Error!! There's a circular dependency";
+    // include job & dependency if they are not already in the final array
+    return `${dependency && !alreadyDependency ? dependency : ''}${acc}${alreadyAJob ? '' : job}`;
+
   }, '');
 
 
