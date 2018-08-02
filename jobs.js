78ea9e2ff678a1575d5aa29fe77978002735f7ab
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-08-02 16:23:49 +0100

    funcionality for self dependency

--- jobs.js
+++ jobs.js
@@ -5,6 +5,13 @@ const sortingJobs = (jobs) => {
   // turn input on array of letters by splitting it along a new line and removing character that is not a letter
   const jobsArr = jobs.split('\n').map(job => job.replace(/\W/g, ''));
 
+  return jobsArr.reduce((acc, [job = '', dependency]) => {
+    if (acc.includes('Error')) return acc;
+    if (job === dependency) return 'Error!!! Jobs cannot depend on themselves';
+  }, '');
+
+
 }
+console.log(sortingJobs('a => a'))
 
 module.exports = { sortingJobs }
