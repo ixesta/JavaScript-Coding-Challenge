b54ec7812f32a64fed2824c0b70be3d983be6369
Ixesta <35632837+ixesta@users.noreply.github.com>
2018-08-02 16:08:28 +0100

    input change

--- jobs.js
+++ jobs.js
@@ -2,6 +2,9 @@ const sortingJobs = (jobs) => {
 
   // returns an empty array when an empty string is passed
   if (jobs.length === 0) return [];
+  // turn input on array of letters by splitting it along a new line and removing character that is not a letter
+  const jobsArr = jobs.split('\n').map(job => job.replace(/\W/g, ''));
+
 }
 
 module.exports = { sortingJobs }
